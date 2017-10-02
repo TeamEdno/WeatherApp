f6a36ae174b50044637ddb05c812f33f1fdd57b6
Atanas Terziev <djnasi4@gmail.com>
2017-10-02 21:57:25 +0300

    added ajaxRequestProviders

---
+++ TeamEdno/scripts/ajaxRequestProvider.js
@@ -0,0 +1,37 @@
+'use strict'
+
+let getCityUrl= (()=>{
+
+    let getSofiaUrl=$.get('api.openweathermap.org/data/2.5/weather?id=2172797');
+
+    let getPlovdivUrl=$.get('api.openweathermap.org/data/2.5/weather?id=728193');
+
+    let getVarnaUrl=$.get('api.openweathermap.org/data/2.5/weather?id=726050');
+
+    let getBurgasUrl=$.get('api.openweathermap.org/data/2.5/weather?id=732770');
+
+    let getRuseUrl=$.get('api.openweathermap.org/data/2.5/weather?id=727523');
+
+    let getStaraZagoraUrl=$.get('api.openweathermap.org/data/2.5/weather?id=726848');
+
+    let getPlevenUrl=$.get('api.openweathermap.org/data/2.5/weather?id=728203');
+
+    let getSlivenUrl=$.get('api.openweathermap.org/data/2.5/weather?id=727079');
+
+    let getDobrichUrl=$.get('api.openweathermap.org/data/2.5/weather?id=726418');
+
+    let getShumenUrl=$.get('api.openweathermap.org/data/2.5/weather?id=727233');
+
+    let getPernikUrl=$.get('api.openweathermap.org/data/2.5/weather?id=728330');
+
+    let getHaskovoUrl=$.get('api.openweathermap.org/data/2.5/weather?id=730435');
+
+    let getYambolUrl=$.get('api.openweathermap.org/data/2.5/weather?id=725578');
+
+    let getPazardzhikUrl=$.get('api.openweathermap.org/data/2.5/weather?id=728378');
+
+    let getBlagoevgradUrl=$.get('api.openweathermap.org/data/2.5/weather?id=733191');
+
+    let getVelikoTurnovoUrl=$.get('api.openweathermap.org/data/2.5/weather?id=725993');
+
+})
