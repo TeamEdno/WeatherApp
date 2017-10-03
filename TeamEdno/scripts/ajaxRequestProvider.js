2822539acfa4846e29cac73804d7b00b84598ecd
radoslavlg <radoslavlgeorgiev@gmail.com>
2017-10-03 10:59:30 +0300

    APPID parameters to ajaxRequestProvider.js; '...WeatherApp\TeamEdno\ForReference(noCode)' folder added

--- TeamEdno/scripts/ajaxRequestProvider.js
+++ TeamEdno/scripts/ajaxRequestProvider.js
@@ -2,36 +2,36 @@
 
 let getCityUrl= (()=>{
 
-    let getSofiaUrl=$.get('api.openweathermap.org/data/2.5/weather?id=2172797');
+    let getSofiaUrl=$.get('api.openweathermap.org/data/2.5/weather?id=2172797&APPID=0d8a1438b002e51fd716aa4e4acafdd7');
 
-    let getPlovdivUrl=$.get('api.openweathermap.org/data/2.5/weather?id=728193');
+    let getPlovdivUrl=$.get('api.openweathermap.org/data/2.5/weather?id=728193&APPID=0d8a1438b002e51fd716aa4e4acafdd7');
 
-    let getVarnaUrl=$.get('api.openweathermap.org/data/2.5/weather?id=726050');
+    let getVarnaUrl=$.get('api.openweathermap.org/data/2.5/weather?id=726050&APPID=0d8a1438b002e51fd716aa4e4acafdd7');
 
-    let getBurgasUrl=$.get('api.openweathermap.org/data/2.5/weather?id=732770');
+    let getBurgasUrl=$.get('api.openweathermap.org/data/2.5/weather?id=732770&APPID=0d8a1438b002e51fd716aa4e4acafdd7');
 
-    let getRuseUrl=$.get('api.openweathermap.org/data/2.5/weather?id=727523');
+    let getRuseUrl=$.get('api.openweathermap.org/data/2.5/weather?id=727523&APPID=0d8a1438b002e51fd716aa4e4acafdd7');
 
-    let getStaraZagoraUrl=$.get('api.openweathermap.org/data/2.5/weather?id=726848');
+    let getStaraZagoraUrl=$.get('api.openweathermap.org/data/2.5/weather?id=726848&APPID=0d8a1438b002e51fd716aa4e4acafdd7');
 
-    let getPlevenUrl=$.get('api.openweathermap.org/data/2.5/weather?id=728203');
+    let getPlevenUrl=$.get('api.openweathermap.org/data/2.5/weather?id=728203&APPID=0d8a1438b002e51fd716aa4e4acafdd7');
 
-    let getSlivenUrl=$.get('api.openweathermap.org/data/2.5/weather?id=727079');
+    let getSlivenUrl=$.get('api.openweathermap.org/data/2.5/weather?id=727079&APPID=0d8a1438b002e51fd716aa4e4acafdd7');
 
-    let getDobrichUrl=$.get('api.openweathermap.org/data/2.5/weather?id=726418');
+    let getDobrichUrl=$.get('api.openweathermap.org/data/2.5/weather?id=726418&APPID=0d8a1438b002e51fd716aa4e4acafdd7');
 
-    let getShumenUrl=$.get('api.openweathermap.org/data/2.5/weather?id=727233');
+    let getShumenUrl=$.get('api.openweathermap.org/data/2.5/weather?id=727233&APPID=0d8a1438b002e51fd716aa4e4acafdd7');
 
-    let getPernikUrl=$.get('api.openweathermap.org/data/2.5/weather?id=728330');
+    let getPernikUrl=$.get('api.openweathermap.org/data/2.5/weather?id=728330&APPID=0d8a1438b002e51fd716aa4e4acafdd7');
 
-    let getHaskovoUrl=$.get('api.openweathermap.org/data/2.5/weather?id=730435');
+    let getHaskovoUrl=$.get('api.openweathermap.org/data/2.5/weather?id=730435&APPID=0d8a1438b002e51fd716aa4e4acafdd7');
 
-    let getYambolUrl=$.get('api.openweathermap.org/data/2.5/weather?id=725578');
+    let getYambolUrl=$.get('api.openweathermap.org/data/2.5/weather?id=725578&APPID=0d8a1438b002e51fd716aa4e4acafdd7');
 
-    let getPazardzhikUrl=$.get('api.openweathermap.org/data/2.5/weather?id=728378');
+    let getPazardzhikUrl=$.get('api.openweathermap.org/data/2.5/weather?id=728378&APPID=0d8a1438b002e51fd716aa4e4acafdd7');
 
-    let getBlagoevgradUrl=$.get('api.openweathermap.org/data/2.5/weather?id=733191');
+    let getBlagoevgradUrl=$.get('api.openweathermap.org/data/2.5/weather?id=733191&APPID=0d8a1438b002e51fd716aa4e4acafdd7');
 
-    let getVelikoTurnovoUrl=$.get('api.openweathermap.org/data/2.5/weather?id=725993');
+    let getVelikoTurnovoUrl=$.get('api.openweathermap.org/data/2.5/weather?id=725993&APPID=0d8a1438b002e51fd716aa4e4acafdd7');
 
 })
