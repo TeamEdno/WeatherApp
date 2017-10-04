0dd90e37895cba2fae3af6c9453a75aabbfe8f1b
radoslavlg <radoslavlgeorgiev@gmail.com>
2017-10-04 12:45:45 +0300

    UL/LI to h4; show/hide buttons

---
+++ TeamEdno/scripts/Cosmetics/ShowHideLogic.js
@@ -0,0 +1,12 @@
+'use strict'
+function showRows24(){
+    $('#showRows24Button').hide();
+    $('#hideRows24Button').show();
+    $('.secondToForthRow').show();
+}
+
+function hideRows24(){
+    $('#showRows24Button').show();
+    $('#hideRows24Button').hide();
+    $('.secondToForthRow').hide();
+}
