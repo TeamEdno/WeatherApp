73f3db942792967558455819e64bce41b02e8564
radoslavlg <radoslavlgeorgiev@gmail.com>
2017-10-07 14:23:57 +0300

    checkForHash function

--- TeamEdno/scripts/Logic/moduleContentGenerator.js
+++ TeamEdno/scripts/Logic/moduleContentGenerator.js
@@ -1,3 +1,22 @@
+let checkForHash = () => {
+  if(window.location.hash){
+    let hash = window.location.hash.slice(1);
+    
+    let cities=bulgarianCities();
+  
+    let cityUrl = getCityUrl(cities,hash);
+  
+    let jsonToHTMLParser=jsonToHTML;
+  
+    let htmlParser = parseGetRequest;
+  
+    let request = pullRequest;
+  
+    request(cityUrl,htmlParser,jsonToHTMLParser);
+  }
+};
+
+checkForHash();
 
 $(window).on('hashchange',()=>{ 
   let hash = window.location.hash.slice(1);
